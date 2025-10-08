dbc4d3563791b8606386a4059afe61dc5c2aeab3
christinalicence <christinalicence@gmail.com>
2025-10-08 15:48:50 +0100

    set debug ready for deployment to heroku

--- football_app/settings.py
+++ football_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
